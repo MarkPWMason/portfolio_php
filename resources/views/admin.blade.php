6dc09cb0fab10635a3c8c5d271058540411b95d0
MarkPWMason <markpwmason@outlook.com>
2023-11-14 17:16:49 +0000

    Small change

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -2,7 +2,7 @@
     <form method="POST" id="adminLogin" action="{{ route('adminLogin') }}">
         @csrf
         <input name="loginusername" class="formInput" type="text">
-        <input name="loginpassword" class="formInput" type="text">
+        <input name="loginpassword" class="formInput" type="password">
         <input class="formBtn" type="submit">
     </form>
 </x-layout>
